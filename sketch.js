d13bcbe78086c5932d7cc4d1872f09ae511cc688
Oran C <oranbusiness@gmail.com>
2018-09-01 13:16:30 -0700

    created starter character class

--- sketch.js
+++ sketch.js
@@ -5,10 +5,27 @@ oranbusiness@gmail.com
 20180901
 github.com/wisehackermonkey
 */
+
+
 function setup() {
-  
+  createCanvas(600,600);
+  background(50);
 }
 
 function draw() {
   
+}
+
+
+function Character(x,y){
+	this.visable = true;
+	this.loc  = createVector(x,y);
+	this.w = 10;
+	this.h = 10;
+	
+	this.show = function(){
+		if(this.visable){
+			rect(this.loc.x,this.loc.y, this.w,this.h);
+		}
+	}
 }
